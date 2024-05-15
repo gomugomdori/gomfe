24c5e5a71deda89d6727f0cb7dd4ffca5c38f1ac
gomugomdori <95206598+gomugomdori@users.noreply.github.com>
2024-05-16 07:56:31 +0900

    update

--- gomfe/src/App.js
+++ gomfe/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is GOMAPP!!</p>
+        <p>This is GOMAPP!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
